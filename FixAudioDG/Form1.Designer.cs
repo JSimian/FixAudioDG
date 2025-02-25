91467a08cea96dd40da4dafa70f276ea98edb2dc
jcbal <jcbal@MONKEY-PC>
2025-02-25 15:22:38 -0300

    Adicionar arquivos de projeto.

---
+++ FixAudioDG/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace FixAudioDG
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(100, 103);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Tempo Real",
+            "Alta",
+            "Acima da Normal",
+            "Normal",
+            "Abaixo da Normal",
+            "Ocioso"});
+            this.comboBox2.Location = new System.Drawing.Point(100, 104);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(142, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Core";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(135, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Prioridade";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 59);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(184, 40);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Aplicar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(295, 52);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Escolha qual será o núcleo responsavel por rodar o AudioDG\r\n\r\nAvalie o uso de seu" +
+    " processador e defina o núcleo que \r\nnormalmente permanece mais ocioso durante s" +
+    "uas tarefas\r\n";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(323, 141);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Afinidade de núcleo";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 156);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(323, 140);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Prioridade";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(11, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(298, 52);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Prioridade a ser aplicada ao AudioDG Inicie em \r\n\'Acima da normal\' e caso necessá" +
+    "rio aplique a prioridade \'Alta\'\r\n\r\nTempo real pode causar anomalias em outros pr" +
+    "ocessos\r\n";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button3);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Location = new System.Drawing.Point(13, 300);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(323, 114);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Automação";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(163, 29);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(89, 24);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Salvar config\r\natual";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(65, 29);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(92, 24);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Carregar config\r\nsalva\r\n";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(10, 422);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Enabled = false;
+            this.label6.Location = new System.Drawing.Point(223, 106);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "*Sugestão";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Enabled = false;
+            this.label7.Location = new System.Drawing.Point(223, 107);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "*Sugestão";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 444);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.groupBox2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Fix AudioDG GUI v.1.0 By Simian";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ FixAudioDG/Form1.cs
@@ -0,0 +1,257 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Windows.Forms;
+//ProcCores = Numero de processadores lógicos no sistema
+
+
+
+namespace FixAudioDG
+{
+    public partial class Form1: Form
+    {
+        //Numero de cores no pc em dec
+        public int ProcCores = Environment.ProcessorCount;
+        //Numero de cores no pc em bin
+        public string TotalCoresBin;
+        //Core selecionado
+        public string StringDecSelectedCore;
+        //core selecionado
+        public int SelectedCore;
+        //zeros a direita do core selecionado
+        public string SelectedCoresPadBin;
+        //bit a ser alterado
+        public string StringBinSelectedCore;
+        //prioridade selecionada
+        public string SelectedPriority;
+        //código da prioridade selecionada
+        public string PrioriCode;
+        //Valida config Salva
+        public int validconf;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            label5.Text = "FixAudioDG GUI v.1.0 by Simian";
+
+            //label1.Text = cores.ToString();
+            for (int i = 0; i < ProcCores; i++)
+            {
+                comboBox1.Items.Add(i);
+            }
+            comboBox1.SelectedIndex = (int)Math.Round((double)(ProcCores / 4 * 3) +1);
+            comboBox2.SelectedIndex = 2;
+            label6.Visible = true;
+            label7.Visible = true;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label6.Visible = false;
+            if (comboBox1.SelectedIndex < (int)Math.Round((double)(ProcCores / 4)))
+            {
+                MessageBox.Show("Evite usar núcleos iniciais, estes tendem a ser mais sobrecarregados", "Recomendação", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            testeconf();
+            if (validconf == 2)
+            {
+                MessageBox.Show("Configuração salva inválida\nSua configuração salva foi redefinida, configure-a novamente", "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            } else if (validconf == 1)
+            {
+                //nada a fazer
+            } else if (validconf == 0)
+            {
+                button2.Enabled = true;
+            }
+            label1.TextAlign = ContentAlignment.MiddleCenter;
+            label2.TextAlign = ContentAlignment.MiddleCenter;
+        }
+
+        private void testeconf()
+        {
+            try
+            {
+                RegistryKey GetReg = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\SimianSoftware\AudioDG");
+
+                string SelCore = GetReg.GetValue("Afinidade").ToString();
+                if (int.Parse(SelCore) < 0 || int.Parse(SelCore) > ProcCores)
+                {
+                    validconf = 2;
+                    GetReg.DeleteValue("Afinidade");
+                    GetReg.DeleteValue("Prioridade");
+                    return;
+                }
+                string SelPrioridade = GetReg.GetValue("Prioridade").ToString();
+                if (int.Parse(SelPrioridade) < 0 || int.Parse(SelPrioridade) > 5)
+                    {
+                    validconf = 2;
+                    GetReg.DeleteValue("Afinidade");
+                    GetReg.DeleteValue("Prioridade");
+                    return;
+                }
+
+                string SelPrio = (GetReg.GetValue("Prioridade").ToString());
+
+                validconf = 0;
+
+            }
+            catch
+            {
+                //MessageBox.Show("Registro não existe");
+                validconf = 1;
+            }
+        }
+
+        private void Afinidade()
+        {
+            //Um zero para cada core do processador do usuario
+            TotalCoresBin = "".PadLeft(ProcCores, '0') + ".";
+            //Core escolhido
+            SelectedCore = comboBox1.SelectedIndex;
+            //um zero a direita de cada core até o core escolhido
+            SelectedCoresPadBin = "".PadLeft(SelectedCore, '0');
+            //altera o zero do core escolhido para 1 e adiciona os zeros a direita
+            StringBinSelectedCore = TotalCoresBin.Replace(SelectedCoresPadBin + ".", "1" + SelectedCoresPadBin).Substring(1);
+            //transforma o binário acima em int decimal e por fim em string
+            StringDecSelectedCore = Convert.ToInt32(StringBinSelectedCore, 2).ToString();
+        }
+
+        private void Prioridade()
+        {
+            /*
+             index  id      Nome
+             0      256      Tempo Real
+             1      128      Alta
+             2      32768    Acima da Normal
+             3      32       Normal
+             4      16384    Abaixo da Normal
+             5      64       Ocioso
+            */
+            SelectedPriority = comboBox2.SelectedIndex.ToString();
+            if (SelectedPriority == "0") { PrioriCode = "256"; }
+            else if (SelectedPriority == "1") { PrioriCode = "128"; }
+            else if (SelectedPriority == "2") { PrioriCode = "32768"; }
+            else if (SelectedPriority == "3") { PrioriCode = "32"; }
+            else if (SelectedPriority == "4") { PrioriCode = "16384"; }
+            else if (SelectedPriority == "5") { PrioriCode = "64"; }
+            else { PrioriCode = "32"; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Afinidade();
+            Prioridade();
+
+           /* //Descomente para debug
+            MessageBox.Show(  "Cores disponiveis em dec: " + ProcCores.ToString() 
+                            + "\nCores disponiveis em Bin: " + TotalCoresBin.ToString() 
+                            + "\nCore Selecionado: " + SelectedCore.ToString() 
+                            + "\nPadding do Core Selecionado em Bin: " + SelectedCoresPadBin
+                            + "\nString Final em Bin: " + StringBinSelectedCore
+                            + "\nString Final em Dec: " + StringDecSelectedCore
+                            + "\n"
+                            + "\nPrioridade index: " + SelectedPriority
+                            + "\nPrioridade code: " + PrioriCode.ToString()
+                            
+            );
+            */
+
+                Go();
+
+        }
+
+        public void Go()
+        {
+            label5.Text = "Definindo afinidade";
+            Process cmda = new Process();
+            cmda.StartInfo.FileName = "powershell.exe";
+            cmda.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmda.StartInfo.Arguments = "-Command \"$process = Get-Process audiodg ; $process.ProcessorAffinity = " + StringDecSelectedCore + "\"";
+            cmda.Start();
+            cmda.WaitForExit();
+            int exitca = cmda.ExitCode;
+            //if (exitca >= 1) { MessageBox.Show("Falhou ao definir Afinidade! \nCodigo de erro: " + exitca, "Falha", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+
+            label5.Text = "Definindo Prioridade";
+            Process cmdb = new Process();
+            cmdb.StartInfo.FileName = "powershell.exe";
+            cmdb.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            cmdb.StartInfo.Arguments = "-Command \"Get-WmiObject Win32_Process -Filter \\\"Name='audiodg.exe'\\\" | ForEach-Object { $_.SetPriority(" + PrioriCode + ") }\"";
+            cmdb.Start();
+            cmdb.WaitForExit();
+            int exitcb = cmdb.ExitCode;
+            //if (exitcb >= 1) { MessageBox.Show("Falhou ao definir Prioridade! \nCodigo de erro: " + exitca, "AudioDG - Falha", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+
+            int exitcsum = (exitca + exitcb);
+            if (exitcsum == 0) { 
+                MessageBox.Show(" Prioridade e afinidade configuradas com Sucesso!", "AudioDG - Sucesso", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                label5.Text = "Sucesso!";
+                //Application.Exit();
+            } else
+            {
+                string error;
+                if (exitca > 0) { error = "Definir Afinidade: Erro " + exitca.ToString(); }else { error = "Definir Afinidade: Sucesso."; }
+                if (exitcb > 0) { error = error + "\nDefinir Prioridade: Erro " + exitcb.ToString(); } else { error = error + "\nDefinir Prioridade: Sucesso."; }
+                MessageBox.Show(error, "Falha", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                label5.Text = "Falhou!";
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            RegistryKey GetReg = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\SimianSoftware\AudioDG");
+
+            string SelCore = GetReg.GetValue("Afinidade").ToString();
+            comboBox1.SelectedIndex = int.Parse(SelCore);
+
+            string SelPrio = (GetReg.GetValue("Prioridade").ToString());
+            comboBox2.SelectedIndex = int.Parse(SelPrio);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Afinidade();
+            Prioridade();
+            RegistryKey newreg = Registry.CurrentUser.CreateSubKey(@"SOFTWARE\SimianSoftware\AudioDG");
+            newreg.SetValue("Prioridade", SelectedPriority, RegistryValueKind.String);
+            newreg.SetValue("Afinidade", SelectedCore, RegistryValueKind.String);
+            testeconf();
+            if (validconf == 0)
+            {
+                button2.Enabled = true;
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            label7.Visible = false;
+            if (comboBox2.SelectedIndex == 3)
+            {
+                MessageBox.Show("Esta configuração não irá surtir efeito na performance do AudioDG", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            if (comboBox2.SelectedIndex > 3)
+            {
+                MessageBox.Show("Esta configuração irá piorar a performance do AudioDG", "Atenção!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
